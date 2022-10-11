2bcf00fdcb61f79a84c5268ddcb718937b91bded
joyjames143 <joyjames143007@gmail.com>
2022-10-12 00:35:04 +0530

    first commit

---
+++ src/server.js
@@ -0,0 +1,63 @@
+import express ,{json} from "express";
+import cors from "cors";
+
+
+import nodemailer from 'nodemailer';
+
+// server used to send send emails
+const router = express.Router();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+
+
+router.post("/contact", async(req, res) => {
+    
+    const reciver_mail = req.body.sender_mail;
+    const reciver_pass = req.body.sender_pass;
+
+    const contactEmail = await nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: reciver_mail,
+          pass: reciver_pass
+        },
+      });
+      
+      await contactEmail.verify((error) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Ready to Send");
+        }
+      });
+
+
+
+  const name = req.body.firstName + req.body.lastName;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  const mail = {
+    from: name,
+    to: reciver_mail,
+    subject: "Contact Form Submission - Portfolio",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ code: 200, status: "Message Sent" });
+    }
+  });
+});
+
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT,()=>{console.log(`server is listining in port : ${PORT}`)}); 
