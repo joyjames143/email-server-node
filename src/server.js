4ab683f4b3a92a26923e82992fd60f6cd95ab927
joyjames143 <joyjames143007@gmail.com>
2022-10-12 00:50:31 +0530

    secong commit

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
-import express ,{json} from "express";
+import express ,{json,Router} from "express";
 import cors from "cors";
 
 
 import nodemailer from 'nodemailer';
 
 // server used to send send emails
-const router = express.Router();
 
+const router = express.Router();
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(json());
 app.use("/", router);
 
 
